8c53272900bb4a8c641d851af88eee160722ae50
rft88 <rodrigotresinari@gmail.com>
2026-04-01 02:08:14 -0300

    teste05

--- src/config/env.js
+++ src/config/env.js
@@ -11,5 +11,7 @@ for (const key of requiredEnvVars) {
 export const env = {
   riotApiKey: process.env.RIOT_API_KEY,
   port: Number(process.env.PORT || 3001),
-  clientOrigin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  clientOrigin: process.env.CLIENT_ORIGIN || "https://next-front-end.onrender.com",
 };
+
+//5173 back 3001 front
